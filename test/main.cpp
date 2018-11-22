d9eaf66e6bb5bb5b6f68a5f8c98cde1436d12ffa
sneppy <andrea.mecchia@mail.polimi.it>
2018-11-22 19:55:38 +0100

    Integrate gtest suite

---
+++ test/main.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+/**
+ * @brief Run all unit tests
+ * 
+ * @return test results
+ */
+int main(int argc, char ** argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
